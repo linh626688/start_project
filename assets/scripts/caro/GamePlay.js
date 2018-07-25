e9e6175198aa8e10eb5bd42e9a25e3aabf3134cf
linh626688@gmail.com <cavoixanh>
2018-07-26 00:24:59 +0700

    add two click

--- assets/scripts/caro/GamePlay.js
+++ assets/scripts/caro/GamePlay.js
@@ -11,57 +11,44 @@ cc.Class({
         table_1: cc.Node,
         table_2: cc.Node,
     },
-    spawnNew: function () {
-
-        var x = 0;
-        var y = 0;
-        for (var i = -7; i < 8; i++) {
-            for (var a = -7; a < 8; a++) {
-
-                var spawn = cc.instantiate(this.blockPrefab);
-                this.node.addChild(spawn);
-                spawn.getComponent('Block').row = a;
-                spawn.getComponent('Block').col = i;
-                x = a * spawn.width;
-                y = i * spawn.height;
-                spawn.setPosition(this.getNewBlockPosition(x, y));
-                // this.table.push(spawn);
-                spawn.getComponent('Block').GamePlay = this;
-
-            }
-        }
-
-    },
     getNewBlockPosition: function (randX, randY) {
 
         return cc.p(randX, randY);
 
     },
-    addTouchListernerTable(table) {
+    addTouchListernerTableFirst() {
         this.node.on(cc.Node.EventType.TOUCH_START, function (event) {
-            let rect = table.getBoundingBoxToWorld();
+            let rect = this.table_1.getBoundingBoxToWorld();
             if (cc.rectContainsPoint(rect, event.getLocation())) {
                 let star = cc.instantiate(this.blockPrefab);
                 star.setPosition(cc.v2(event.getLocation().x - 480, event.getLocation().y - 320));
                 this.node.addChild(star);
+                console.log("star 1" ,star.getPosition());
 
-                // let allChildren = this.table.getChildren();                        
-                // cc.log(rect);
-                // let ls
-                // for(let i = 0; i < allChildren.length; i++){
-                //     let rect = allChildren[i].getBoundingBoxToWorld();    
-                //     if (cc.rectContainsPoint(rect, event.getLocation())) {
-                //         cc.log(allChildren[i].getName());
-                //         return true;
-                // }
+                let star2 = cc.instantiate(this.blockPrefab);
+                star2.setPosition(cc.v2(event.getLocation().x - 480 , event.getLocation().y));
+                this.node.addChild(star2);
+                console.log("star 2" ,star2.getPosition())
+            }
+        }, this);
+    },
+    addTouchListernerTableSecond() {
+        this.node.on(cc.Node.EventType.TOUCH_START, function (event) {
+            let rect = this.table_2.getBoundingBoxToWorld();
+            if (cc.rectContainsPoint(rect, event.getLocation())) {
+                let star = cc.instantiate(this.blockPrefab);
+                star.setPosition(cc.v2(event.getLocation().x - 480  , event.getLocation().y - 320));
+                this.node.addChild(star);
 
+                let star2 = cc.instantiate(this.blockPrefab);
+                star2.setPosition(cc.v2(event.getLocation().x- 480 , event.getLocation().y - 640 ));
+                this.node.addChild(star2);
             }
         }, this);
     },
     onLoad() {
-        this.addTouchListernerTable(this.table_1);
-
-        this.addTouchListernerTable(this.table_2);
+        this.addTouchListernerTableFirst();
+        this.addTouchListernerTableSecond();
     },
 
     start() {
--- library/imports/32/32f9158b-cb79-4740-bc68-3fc86eabd9ad.js
+++ library/imports/32/32f9158b-cb79-4740-bc68-3fc86eabd9ad.js
@@ -2,7 +2,7 @@
 cc._RF.push(module, '32f91WLy3lHQLxoP8huq9mt', 'GamePlay');
 // scripts/caro/GamePlay.js
 
-'use strict';
+"use strict";
 
 cc.Class({
     extends: cc.Component,
@@ -15,53 +15,43 @@ cc.Class({
         table_1: cc.Node,
         table_2: cc.Node
     },
-    spawnNew: function spawnNew() {
-
-        var x = 0;
-        var y = 0;
-        for (var i = -7; i < 8; i++) {
-            for (var a = -7; a < 8; a++) {
-
-                var spawn = cc.instantiate(this.blockPrefab);
-                this.node.addChild(spawn);
-                spawn.getComponent('Block').row = a;
-                spawn.getComponent('Block').col = i;
-                x = a * spawn.width;
-                y = i * spawn.height;
-                spawn.setPosition(this.getNewBlockPosition(x, y));
-                // this.table.push(spawn);
-                spawn.getComponent('Block').GamePlay = this;
-            }
-        }
-    },
     getNewBlockPosition: function getNewBlockPosition(randX, randY) {
 
         return cc.p(randX, randY);
     },
-    addTouchListernerTable: function addTouchListernerTable(table) {
+    addTouchListernerTableFirst: function addTouchListernerTableFirst() {
         this.node.on(cc.Node.EventType.TOUCH_START, function (event) {
-            var rect = table.getBoundingBoxToWorld();
+            var rect = this.table_1.getBoundingBoxToWorld();
+            if (cc.rectContainsPoint(rect, event.getLocation())) {
+                var star = cc.instantiate(this.blockPrefab);
+                star.setPosition(cc.v2(event.getLocation().x - 480, event.getLocation().y - 320));
+                this.node.addChild(star);
+                console.log("star 1", star.getPosition());
+
+                var star2 = cc.instantiate(this.blockPrefab);
+                star2.setPosition(cc.v2(event.getLocation().x - 480, event.getLocation().y));
+                this.node.addChild(star2);
+                console.log("star 2", star2.getPosition());
+            }
+        }, this);
+    },
+    addTouchListernerTableSecond: function addTouchListernerTableSecond() {
+        this.node.on(cc.Node.EventType.TOUCH_START, function (event) {
+            var rect = this.table_2.getBoundingBoxToWorld();
             if (cc.rectContainsPoint(rect, event.getLocation())) {
                 var star = cc.instantiate(this.blockPrefab);
                 star.setPosition(cc.v2(event.getLocation().x - 480, event.getLocation().y - 320));
                 this.node.addChild(star);
 
-                // let allChildren = this.table.getChildren();                        
-                // cc.log(rect);
-                // let ls
-                // for(let i = 0; i < allChildren.length; i++){
-                //     let rect = allChildren[i].getBoundingBoxToWorld();    
-                //     if (cc.rectContainsPoint(rect, event.getLocation())) {
-                //         cc.log(allChildren[i].getName());
-                //         return true;
-                // }
+                var star2 = cc.instantiate(this.blockPrefab);
+                star2.setPosition(cc.v2(event.getLocation().x - 480, event.getLocation().y - 640));
+                this.node.addChild(star2);
             }
         }, this);
     },
     onLoad: function onLoad() {
-        this.addTouchListernerTable(this.table_1);
-
-        this.addTouchListernerTable(this.table_2);
+        this.addTouchListernerTableFirst();
+        this.addTouchListernerTableSecond();
     },
     start: function start() {
         // this.addTouchList)erner();
